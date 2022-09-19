bc0c11ca32ec52e11c96fdc302ce112494215465
Matt Pharr <matt@pharr.org>
2022-09-19 06:52:00 -0700

    try 1 at signing osx app in github build actions

--- osx/make-osx-app.sh
+++ osx/make-osx-app.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh -x
 
 set -e
 
@@ -16,8 +16,10 @@ if [[ ! -v VERSION ]]; then
     echo VERSION environment variable must be set
     exit 1
 fi
-    
+
 cd osx
+
+# start creating Vice.app    
 rm -rf Vice.app
 mkdir -p Vice.app/Contents/MacOS
 cp Info.plist Vice.App/Contents
@@ -40,12 +42,59 @@ cp ../icons/tower-rounded-inset-512x512.png icon.iconset/icon_512x512.png
 cp ../icons/tower-rounded-inset-1024x1024.png icon.iconset/icon_512x512@2.png
 iconutil -c icns icon.iconset
 mkdir -p Vice.app/Contents/Resources
-/bin/mv icon.icns Vice.app/Contents/Resources
+cp icon.icns Vice.app/Contents/Resources
 
-# get the code signed and create a dmg file
-gon -log-level=info  gon-config.hcl
+# sign the app
+codesign -s "${APPLE_DEVELOPER_ID_APPLICATION}" -f -v --timestamp --options runtime Vice.app
+
+# make a zip for notarization
+zip -rv vice.zip Vice.app
+
+# get the zip file notarized
+xcrun altool --notarize-app \
+      --primary-bundle-id org.pharr.vice \
+      -u ${AC_USERNAME} \
+      -p @env:APPLE_CODESIGN_PASSWORD \
+      -f vice.zip > notarize.out
+
+# via https://www.msweet.org/blog/2020-12-10-macos-notarization.html
+uuid=`grep RequestUUID notarize.out | awk '{print $3}'`
+notarizestatus="in progress"
+
+while [[ "$notarizestatus" = "in progress" ]]; do
+  echo "    $uuid: $notarizestatus"
+  sleep 10
+  echo "xcrun altool --notarization-info ..."
+  xcrun altool --notarization-info $uuid -u ${AC_USERNAME} --password @env:APPLE_CODESIGN_PASSWORD > notarize.out
+  cat notarize.out
+  notarizestatus="`grep Status: notarize.out | cut -b14-`"
+done
+
+echo "    $uuid: $notarizestatus"
+
+# notarized! staple the notarization
+xcrun stapler staple Vice.app
+
+# create dmg
+/bin/rm -f Vice.dmg
+~/create-dmg/create-dmg --volname "Vice-${VERSION}" \
+           --volicon icon.icns \
+           --icon-size 100 \
+           --icon "Vice.app" 10 180 \
+           --app-drop-link 350 180 \
+           --window-size 570 420 \
+           --background dmg-background.png  \
+           Vice.dmg \
+           Vice.app
+
+rm vice.zip
+
+# on altool fail, do:
+# xcrun altool --notarization-history 0 --username ${AC_USERNAME} --password ${APPLE_CODESIGN_PASSWORD}
+# then:
+# xcrun altool --notarization-info {REQUESTUUID} --username ${AC_USERNAME} --password ${APPLE_CODESIGN_PASSWORD}
+# to get URL for logs...
 
 # upload it to github
 mv Vice.dmg Vice-${VERSION}.dmg
-#gh release create ${VERSION}
-gh release upload ${VERSION} Vice-${VERSION}.dmg
+#gh release upload ${VERSION} Vice-${VERSION}.dmg
