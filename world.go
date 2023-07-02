69514a99f20e9683d02598bbb85fb3d08bb30439
Matt Pharr <matt@pharr.org>
2023-07-02 11:29:17 +0200

    Adjust world update fetch rate based on sim rate

--- world.go
+++ world.go
@@ -445,7 +445,9 @@ func (w *World) GetUpdates(eventStream *EventStream, onErr func(error)) {
 
 	w.checkPendingRPCs(eventStream)
 
-	if time.Since(w.lastUpdate) > 1*time.Second {
+	// Wait in seconds between update fetches; no less than 100ms
+	rate := min(0.1, 1/w.SimRate)
+	if time.Since(w.lastUpdate) > time.Duration(rate*float32(time.Second)) {
 		if w.updateCall != nil {
 			lg.Errorf("Still waiting on last update call!")
 			return
