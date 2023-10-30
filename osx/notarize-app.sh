6a1f39b9860a767ce9d2039b8611c2d6eed94ce7
Matt Pharr <matt@pharr.org>
2023-10-30 07:03:22 -0700

    Attempt to update OSX notarization to notarytool

--- osx/notarize-app.sh
+++ osx/notarize-app.sh
@@ -6,25 +6,12 @@ set -e
 zip -rv vice.zip Vice.app
 
 # get the zip file notarized
-xcrun altool --notarize-app \
-      --primary-bundle-id org.pharr.vice \
-      -u ${APPLE_CODESIGN_ID} \
-      -p @env:APPLE_CODESIGN_PASSWORD \
-      -f vice.zip > notarize.out
-
-# via https://www.msweet.org/blog/2020-12-10-macos-notarization.html
-uuid=`grep RequestUUID notarize.out | awk '{print $3}'`
-notarizestatus="in progress"
-
-while [[ "$notarizestatus" = "in progress" ]]; do
-  echo "    $uuid: $notarizestatus"
-  sleep 10
-  echo "xcrun altool --notarization-info ..."
-  xcrun altool --notarization-info $uuid -u ${APPLE_CODESIGN_ID} --password @env:APPLE_CODESIGN_PASSWORD > notarize.out
-  notarizestatus="`grep Status: notarize.out | sed 's/^.*Status: //'`"
-done
-
-echo "    $uuid: $notarizestatus"
+xcrun notarytool submit \
+      --wait \
+      --apple-id ${APPLE_CODESIGN_ID} \
+      --password @env:APPLE_CODESIGN_PASSWORD \
+      --timeout 5m \
+      vice.zip
 
 # notarized! staple the notarization
 xcrun stapler staple Vice.app
