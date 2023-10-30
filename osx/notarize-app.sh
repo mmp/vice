abd9b505a2249106209d6de00046a9122a207ab1
Matt Pharr <matt@pharr.org>
2023-10-30 14:57:17 -0700

    Revert "Try 2 on notarytool"
    This reverts commit fe6b437429012a9205dde844ec05978a80e5b0f1.

--- osx/notarize-app.sh
+++ osx/notarize-app.sh
@@ -10,7 +10,6 @@ xcrun notarytool submit \
       --wait \
       --apple-id ${APPLE_CODESIGN_ID} \
       --password @env:APPLE_CODESIGN_PASSWORD \
-      --team-id 325U8G3LXB \
       --timeout 5m \
       vice.zip
 
