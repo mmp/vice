e9416219f76559ca448c2904421660077ccc26ed
Matt Pharr <matt@pharr.org>
2024-04-05 16:46:04 -0700

    STARSPane: small improvements to scratchpad validation
    - For implied (i.e. "enter it and slew"), 2-character scratchpads that match active
      TCP IDs aren't allowed for the primary scratchpad
    - Secondary must be >= 2 characters, primary >= 1
    - Can't be 3 numerals
    - The various forbidden strings should be matched against the first 3 characters
      of 4-character scratchpads
    - Rename STARSFacilityAdaptation ScratchpadRules to AllowLongScratchpads

--- scenario.go
+++ scenario.go
@@ -57,17 +57,17 @@ type AirspaceAwareness struct {
 }
 
 type STARSFacilityAdaptation struct {
-	AirspaceAwareness []AirspaceAwareness   `json:"airspace_awareness"`
-	ForceQLToSelf     bool                  `json:"force_ql_self"`
-	ScratchpadRules   [2]bool               `json:"allow_long_scratchpad"` // [0] is for the primary. [1] is for the secondary
-	Maps              []STARSMap            `json:"stars_maps"`
-	InhibitCAVolumes  []AirspaceVolume      `json:"inhibit_ca_volumes"`
-	RadarSites        map[string]*RadarSite `json:"radar_sites"`
-	Center            Point2LL              `json:"-"`
-	CenterString      string                `json:"center"`
-	Range             float32               `json:"range"`
-	Scratchpads       map[string]string     `json:"scratchpads"`
-	VideoMapFile      string                `json:"video_map_file"`
+	AirspaceAwareness   []AirspaceAwareness   `json:"airspace_awareness"`
+	ForceQLToSelf       bool                  `json:"force_ql_self"`
+	AllowLongScratchpad [2]bool               `json:"allow_long_scratchpad"` // [0] is for the primary. [1] is for the secondary
+	Maps                []STARSMap            `json:"stars_maps"`
+	InhibitCAVolumes    []AirspaceVolume      `json:"inhibit_ca_volumes"`
+	RadarSites          map[string]*RadarSite `json:"radar_sites"`
+	Center              Point2LL              `json:"-"`
+	CenterString        string                `json:"center"`
+	Range               float32               `json:"range"`
+	Scratchpads         map[string]string     `json:"scratchpads"`
+	VideoMapFile        string                `json:"video_map_file"`
 }
 
 type Airspace struct {
