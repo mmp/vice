98d12a7fd9d6e497a6a06253348e507e7cd4dfd5
Matt Pharr <matt@pharr.org>
2024-04-11 21:14:32 -0400

    STARSPane: draw leader lines before radar tracks

--- stars.go
+++ stars.go
@@ -1573,8 +1573,8 @@ func (sp *STARSPane) Draw(ctx *PaneContext, cb *CommandBuffer) {
 
 	DrawHighlighted(ctx, transforms, cb)
 
-	sp.drawTracks(aircraft, ctx, transforms, cb)
 	sp.drawLeaderLines(aircraft, ctx, transforms, cb)
+	sp.drawTracks(aircraft, ctx, transforms, cb)
 	sp.drawDatablocks(aircraft, ctx, transforms, cb)
 
 	ghosts := sp.getGhostAircraft(aircraft, ctx)
