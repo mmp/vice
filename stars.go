ec3b7847d6a06dc2b44278b082dfacd591bdfdab
Michael <michaelt96099@gmail.com>
2024-05-09 21:40:27 -0400

    Track zoom?

--- stars.go
+++ stars.go
@@ -5392,13 +5392,25 @@ func (sp *STARSPane) drawTracks(aircraft []*Aircraft, ctx *PaneContext, transfor
 	transforms.LoadLatLongViewingMatrices(cb)
 	cb.PointSize(5)
 	pd.GenerateCommands(cb)
-	cb.PointSize(12) // bigger points for fused mode primary tracks
+	cb.PointSize(sp.getTrackSize(ctx, transforms)) // bigger points for fused mode primary tracks
+
 	pd2.GenerateCommands(cb)
 	trid.GenerateCommands(cb)
 	cb.LineWidth(1)
 	ld.GenerateCommands(cb)
 	transforms.LoadWindowViewingMatrices(cb)
 	td.GenerateCommands(cb)
+	
+}
+
+func (sp *STARSPane) getTrackSize(ctx *PaneContext, transforms ScopeTransformations) float32 {
+	var output float32 = 13 // base track size
+	e := transforms.PixelDistanceNM(ctx.world.NmPerLongitude)
+	var distance float32 = 0.3623 // Around 2200 feet in nm
+	if distance / e > 13 {
+		output = distance / e
+	}
+	return output
 }
 
 func (sp *STARSPane) getGhostAircraft(aircraft []*Aircraft, ctx *PaneContext) []*GhostAircraft {
@@ -6616,7 +6628,6 @@ func (sp *STARSPane) drawRingsAndCones(aircraft []*Aircraft, ctx *PaneContext, t
 				td.AddText(format(state.JRingRadius), pt, textStyle)
 			}
 		}
-
 		atpaStatus := state.ATPAStatus // this may change
 
 		// If warning/alert cones are inhibited but monitor cones are not,
