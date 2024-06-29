082dc539a50f5a4c4a90ceb8a5e220846312214b
Matt Pharr <matt@pharr.org>
2024-06-29 22:13:26 +0100

    Update credits. (CC PR #264)

--- ui.go
+++ ui.go
@@ -1110,12 +1110,12 @@ func showAboutDialog() {
   Valencia, and Yi Zhang.
 - Facility engineering: Connor Allen, Adam
   Bolek, Lucas Chan, Aaron Flett, Mike K,
-  Jonah Lefkoff, Jud Lopez, Ethan Malimon,
-  Jace Martin, Merry, Yahya Nazimuddin,
-  Justin Nguyen, Giovanni, Andrew S,
-  Arya T, Nelson T, Eli Thompson, Michael
-  Trokel, Samuel Valencia, Gavin Velicevic,
-  and Jackson Verdoorn.
+  Josh Lambert, Jonah Lefkoff, Jud Lopez,
+  Ethan Malimon, Jace Martin, Merry,
+  Yahya Nazimuddin, Justin Nguyen, Giovanni,
+  Andrew S, Arya T, Nelson T, Eli Thompson,
+  Michael Trokel, Samuel Valencia, Gavin
+  Velicevic, and Jackson Verdoorn.
 - Video maps: thanks to the ZAU, ZBW, ZDC,
   ZDV, ZHU, ZID, ZJX, ZLA, ZMP, ZNY, ZOB,
   ZSE, and ZTL VATSIM ARTCCs.
