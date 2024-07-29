367f44a81c02bda3392376b9c99394a67ec11e87
Matt Pharr <matt@pharr.org>
2024-07-29 06:22:53 -0700

    STARSPane: draw bevels on DCB buttons more accurately

--- pkg/panes/stars/dcb.go
+++ pkg/panes/stars/dcb.go
@@ -661,19 +661,33 @@ func drawDCBButton(ctx *panes.Context, text string, flags int, buttonScale float
 	trid.AddQuad(p0, p1, p2, p3, buttonColor)
 	drawDCBText(text, td, sz, textColor)
 
+	// Draw the bevel around the button
+	topLeftBevelColor := renderer.RGB{0.2, 0.2, 0.2}
+	bottomRightBevelColor := renderer.RGB{0, 0, 0}
+	shiftp := func(p [2]float32, dx, dy float32) [2]float32 {
+		return math.Add2f(p, [2]float32{dx, dy})
+	}
 	if !disabled && pushedIn { //((selected && !mouseInside) || (!selected && mouseInside && mouse.Down[MouseButtonPrimary])) {
 		// Depressed bevel scheme: darker top/left, highlight bottom/right
-		ld.AddLine(p0, p1, renderer.LerpRGB(.5, buttonColor, renderer.RGB{0, 0, 0}))
-		ld.AddLine(p0, p3, renderer.LerpRGB(.5, buttonColor, renderer.RGB{0, 0, 0}))
-		ld.AddLine(p1, p2, renderer.LerpRGB(.25, buttonColor, renderer.RGB{1, 1, 1}))
-		ld.AddLine(p2, p3, renderer.LerpRGB(.25, buttonColor, renderer.RGB{1, 1, 1}))
-	} else {
-		// Normal bevel scheme: highlight top and left, darker bottom and right
-		ld.AddLine(p0, p1, renderer.LerpRGB(.25, buttonColor, renderer.RGB{1, 1, 1}))
-		ld.AddLine(p0, p3, renderer.LerpRGB(.25, buttonColor, renderer.RGB{1, 1, 1}))
-		ld.AddLine(p1, p2, renderer.LerpRGB(.5, buttonColor, renderer.RGB{0, 0, 0}))
-		ld.AddLine(p2, p3, renderer.LerpRGB(.5, buttonColor, renderer.RGB{0, 0, 0}))
+		topLeftBevelColor, bottomRightBevelColor = bottomRightBevelColor, topLeftBevelColor
 	}
+	// Draw the bevel via individual 1-pixel lines (note that down is negative y...)
+	// Top, with the right end pulled left
+	ld.AddLine(p0, p1, topLeftBevelColor)
+	ld.AddLine(shiftp(p0, 0, -1), shiftp(p1, -1, -1), topLeftBevelColor)
+	ld.AddLine(shiftp(p0, 0, -2), shiftp(p1, -2, -2), topLeftBevelColor)
+	// Left side with bottom end pulled up
+	ld.AddLine(p0, p3, topLeftBevelColor)
+	ld.AddLine(shiftp(p0, 1, 0), shiftp(p3, 1, 1), topLeftBevelColor)
+	ld.AddLine(shiftp(p0, 2, 0), shiftp(p3, 2, 2), topLeftBevelColor)
+	// Right side with top pulled down
+	ld.AddLine(p1, p2, bottomRightBevelColor)
+	ld.AddLine(shiftp(p1, -1, -1), shiftp(p2, -1, 0), bottomRightBevelColor)
+	ld.AddLine(shiftp(p1, -2, -2), shiftp(p2, -2, 0), bottomRightBevelColor)
+	// Bottom with left end pulled right
+	ld.AddLine(p2, p3, bottomRightBevelColor)
+	ld.AddLine(shiftp(p2, 0, 1), shiftp(p3, 1, 1), bottomRightBevelColor)
+	ld.AddLine(shiftp(p2, 0, 2), shiftp(p3, 2, 2), bottomRightBevelColor)
 
 	// Scissor to just the extent of the button. Note that we need to give
 	// this in window coordinates, not our local pane coordinates, so
