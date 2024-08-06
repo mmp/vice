1fcddb3b21a238aca244adddd20216ab8a298989
Matt Pharr <matt@pharr.org>
2024-08-06 12:51:36 -0700

    stars/fonts.go: rework handling of windows high DPI to not panic
    The STARS cursors aren't all < 32x32 after doubling, which led to a panic.
    Now we just pixel double if needed in STARSGlyph rasterize().
    
    Fixes #308.

--- pkg/panes/stars/fonts.go
+++ pkg/panes/stars/fonts.go
@@ -28,48 +28,11 @@ func createFontAtlas(r renderer.Renderer, p platform.Platform) []*renderer.Font 
 	// accordingly. Here we just double up pixels since we want to maintain
 	// the realistic chunkiness of the original fonts.
 	doublePixels := runtime.GOOS == "windows" && p.DPIScale() > 1.5
-
-	doubleSTARSFont := func(sf STARSFont) STARSFont {
-		for i := range sf.Glyphs {
-			g := &sf.Glyphs[i]
-			g.StepX *= 2
-			g.Bounds[0] *= 2
-			g.Bounds[1] *= 2
-
-			// Generate a new bitmap with 2x as many
-			// pixels. Fortunately the original bitmaps are all under
-			// 16 pixels wide, so they will still fit in an uint32.
-			var bitmap []uint32
-			for _, line := range g.Bitmap {
-				if line&0xffff != 0 {
-					panic("not enough room in 32 bits")
-				}
-
-				// Horizontal doubling: double all of the set bits in
-				// the line.
-				var newLine uint32
-				for b := 0; b < 32; b++ {
-					// 0b_abcdefghijklmnop0000000000000000 ->
-					// 0b_aabbccddeeffgghhiijjkkllmmnnoopp
-					if line&(1<<(b/2+16)) != 0 {
-						newLine |= 1 << b
-					}
-				}
-
-				// Vertical doubling: add the line twice to the bitmap.
-				bitmap = append(bitmap, newLine, newLine)
-			}
-			g.Bitmap = bitmap
-		}
-		return sf
-	}
+	scale := 1
 
 	if doublePixels {
 		res *= 2
-		for name, sf := range starsFonts {
-			starsFonts[name] = doubleSTARSFont(sf)
-		}
-		starsCursors = doubleSTARSFont(starsCursors)
+		scale = 2
 	}
 
 	atlas := image.NewRGBA(image.Rectangle{Max: image.Point{X: res, Y: res}})
@@ -80,33 +43,32 @@ func createFontAtlas(r renderer.Renderer, p platform.Platform) []*renderer.Font 
 	addFontToAtlas := func(fontName string, sf STARSFont) {
 		id := renderer.FontIdentifier{
 			Name: fontName,
-			Size: util.Select(doublePixels, sf.Height/2, sf.Height),
+			Size: sf.Height,
 		}
 
-		f := renderer.MakeFont(sf.Height, true /* mono */, id, nil)
+		f := renderer.MakeFont(scale*sf.Height, true /* mono */, id, nil)
 		newFonts = append(newFonts, f)
 
-		if y+sf.Height >= res {
+		if y+scale*sf.Height >= res {
 			panic("STARS font atlas texture too small")
 		}
 
 		for ch, glyph := range sf.Glyphs {
-			if x+glyph.StepX+1 > res {
+			if x+scale*glyph.StepX+1 > res {
 				// Start a new line
 				x = 0
-				y += sf.Height + 1
+				y += scale*sf.Height + 1
 			}
 
-			glyph.rasterize(atlas, x, y)
-			glyph.addToFont(ch, x, y, res, f)
+			glyph.rasterize(atlas, x, y, scale)
+			glyph.addToFont(ch, x, y, res, f, scale)
 
-			x += glyph.StepX + 1 /* pad */
+			x += scale*glyph.StepX + 1 /* pad */
 		}
 
 		// Start a new line after finishing a font.
 		x = 0
-		y += sf.Height + 1
-
+		y += scale*sf.Height + 1
 	}
 
 	// Iterate over the fonts, create Font/Glyph objects for them, and copy
@@ -124,7 +86,7 @@ func createFontAtlas(r renderer.Renderer, p platform.Platform) []*renderer.Font 
 	return newFonts
 }
 
-func (glyph STARSGlyph) rasterize(img *image.RGBA, dx, dy int) {
+func (glyph STARSGlyph) rasterize(img *image.RGBA, x0, y0 int, scale int) {
 	// STARSGlyphs store their bitmaps as an array of uint32s, where each
 	// uint32 encodes a scanline and bits are set in it to indicate that
 	// the corresponding pixel should be drawn; thus, there are no
@@ -136,23 +98,27 @@ func (glyph STARSGlyph) rasterize(img *image.RGBA, dx, dy int) {
 			mask := uint32(1 << (31 - x))
 			if line&mask != 0 {
 				on := color.RGBA{R: 255, G: 255, B: 255, A: 255}
-				img.SetRGBA(x+dx, y+dy, on)
+				for dy := range scale {
+					for dx := range scale {
+						img.SetRGBA(x0+scale*x+dx, y0+scale*y+dy, on)
+					}
+				}
 			}
 		}
 	}
 }
 
-func (glyph STARSGlyph) addToFont(ch, x, y, res int, f *renderer.Font) {
+func (glyph STARSGlyph) addToFont(ch, x, y, res int, f *renderer.Font, scale int) {
 	g := &renderer.Glyph{
 		X0:       0,
-		X1:       float32(glyph.Bounds[0]),
+		X1:       float32(scale * glyph.Bounds[0]),
 		Y0:       0,
-		Y1:       float32(glyph.Bounds[1]),
+		Y1:       float32(scale * glyph.Bounds[1]),
 		U0:       float32(x) / float32(res),
 		V0:       float32(y) / float32(res),
-		U1:       (float32(x + glyph.Bounds[0])) / float32(res),
-		V1:       (float32(y + glyph.Bounds[1])) / float32(res),
-		AdvanceX: float32(glyph.StepX),
+		U1:       (float32(x + scale*glyph.Bounds[0])) / float32(res),
+		V1:       (float32(y + scale*glyph.Bounds[1])) / float32(res),
+		AdvanceX: float32(scale * glyph.StepX),
 		Visible:  true,
 	}
 	f.AddGlyph(ch, g)
