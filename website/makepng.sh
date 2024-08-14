8f861491fe922d475788462a4a18bb0611e7a689
Matt Pharr <matt@pharr.org>
2024-08-14 11:17:33 -0700

    WIP documentation updates, release notes

--- website/makepng.sh
+++ website/makepng.sh
@@ -5,10 +5,13 @@ if [[ $# -ne 1 ]]; then
 fi
 
 fn=$1
-ss=`ls -t ~/Desktop/Screenshot* | head -1`
+#ss=`ls -t ~/Desktop/Screenshot* | head -1`
+ss=~/capture.png
 
-mv ${ss} ${fn}-2x.png
+#convert -shave 2x2 ${ss} ${fn}-2x.png
+convert ${ss} ${fn}-2x.png
 convert -scale 50% ${fn}-2x.png ${fn}.png
+rm ${ss}
 
 w=`file ${fn}.png | awk '{print $5}'`
 h=`file ${fn}.png | awk '{print $7}' | sed s/,//`
