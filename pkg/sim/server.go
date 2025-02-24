2c65eb2271e8902884e2f34f2113d903adb2faf1
Matt Pharr <matt@pharr.org>
2025-02-24 07:52:02 -0800

    sim: bump RPC version (and revert base port to 8000)
    Multiple RPC-related additions since the release (and likely more to
    come..)

--- pkg/sim/server.go
+++ pkg/sim/server.go
@@ -24,8 +24,8 @@ import (
 )
 
 const ViceServerAddress = "vice.pharr.org"
-const ViceServerPort = 8080 + ViceRPCVersion
-const ViceRPCVersion = 22
+const ViceServerPort = 8000 + ViceRPCVersion
+const ViceRPCVersion = 23
 
 type Server struct {
 	*util.RPCClient
