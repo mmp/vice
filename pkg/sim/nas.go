mmp/vice

commit e7a9746eb9e801132a339c2259f4b2f6b68b1b87

sim: rework beacon code assignment to always go through ERAMComputer
RIP "squawk := av.Squawk(rand.Intn(0o7000))"

pkg/sim/nas.go
@@ -110,6 +110,10 @@ func (comp *ERAMComputer) CreateSquawk() (av.Squawk, error) {
 	return comp.SquawkCodePool.Get()
 }
 
+func (comp *ERAMComputer) ReturnSquawk(code av.Squawk) error {
+	return comp.SquawkCodePool.Return(code)
+}
+
 func (comp *ERAMComputer) SendFlightPlans(tracon string, simTime time.Time, lg *log.Logger) {
 	// FIXME(mtrokel): does this need to remove plans from comp.FlightPlans
 	// / comp.TrackInformation after sending them?
@@ -448,6 +452,10 @@ func (comp *STARSComputer) Activate(pool *av.SquawkCodePool) {
 // For local codes
 func (comp *STARSComputer) CreateSquawk() (av.Squawk, error) {
 	return comp.SquawkCodePool.Get()
+}
+
+func (comp *STARSComputer) ReturnSquawk(code av.Squawk) error {
+	return comp.SquawkCodePool.Return(code)
 }
 
 func (comp *STARSComputer) SendTrackInfo(receivingFacility string, msg FlightPlanMessage, simTime time.Time) {

pkg/sim/sim.go
@@ -3544,8 +3544,6 @@ func (ss *State) sampleAircraft(al av.AirlineSpecifier, lg *log.Logger) (*av.Air
 		}
 	}
 
-	squawk := av.Squawk(rand.Intn(0o7000))
-
 	acType := aircraft
 	if perf.WeightClass == "H" {
 		acType = "H/" + acType
@@ -3556,7 +3554,6 @@ func (ss *State) sampleAircraft(al av.AirlineSpecifier, lg *log.Logger) (*av.Air
 
 	return &av.Aircraft{
 		Callsign: callsign,
-		Squawk:   squawk,
 		Mode:     av.Altitude,
 	}, acType
 }
@@ -3589,7 +3586,11 @@ func (s *Sim) createArrivalNoLock(group string, arrivalAirport string) (*av.Airc
 		return nil, fmt.Errorf("unable to sample a valid aircraft")
 	}
 
-	// ac.Squawk = artcc.CreateSquawk()
+	sq, err := s.State.ERAMComputer().CreateSquawk()
+	if err != nil {
+		return nil, err
+	}
+	ac.Squawk = sq
 	ac.FlightPlan = ac.NewFlightPlan(av.IFR, acType, airline.Airport, arrivalAirport)
 
 	// Figure out which controller will (for starters) get the arrival
@@ -3675,8 +3676,15 @@ func (s *Sim) createDepartureNoLock(departureAirport, runway, category string) (
 		} else if r < .03 {
 			ac.Mode = av.Standby // flat out off
 		}
+	} else {
+		sq, err := s.State.ERAMComputer().CreateSquawk()
+		if err != nil {
+			return nil, err
+		}
+		ac.Squawk = sq
 	}
 	ac.FlightPlan = ac.NewFlightPlan(rules, acType, departureAirport, dep.Destination)
+
 	exitRoute := rwy.ExitRoutes[dep.Exit]
 	if err := ac.InitializeDeparture(ap, departureAirport, dep, runway, *exitRoute,
 		s.State.NmPerLongitude, s.State.MagneticVariation, s.State.Scratchpads,
@@ -3718,6 +3726,12 @@ func (s *Sim) createOverflightNoLock(group string) (*av.Aircraft, error) {
 		} else if r < .03 {
 			ac.Mode = av.Standby // flat out off
 		}
+	} else {
+		sq, err := s.State.ERAMComputer().CreateSquawk()
+		if err != nil {
+			return nil, err
+		}
+		ac.Squawk = sq
 	}
 	ac.FlightPlan = ac.NewFlightPlan(rules, acType, airline.DepartureAirport,
 		airline.ArrivalAirport)
@@ -3745,15 +3759,6 @@ func (s *Sim) createOverflightNoLock(group string) (*av.Aircraft, error) {
 		s.State /* wind */, s.lg); err != nil {
 		return nil, err
 	}
-
-	// TODO(mtrokel)
-	/*
-			facility, ok := s.State.FacilityFromController(ac.TrackingController)
-			if !ok {
-				return nil, ErrUnknownControllerFacility
-			}
-		    s.State.ERAMComputers.AddArrival(ac, facility, s.State.STARSFacilityAdaptation, s.SimTime)
-	*/
 
 	return ac, nil
 }

pkg/sim/state.go
@@ -486,6 +486,7 @@ func (ss *State) FacilityFromController(callsign string) (string, bool) {
 
 func (ss *State) DeleteAircraft(ac *av.Aircraft) {
 	delete(ss.Aircraft, ac.Callsign)
+	ss.ERAMComputer().ReturnSquawk(ac.Squawk)
 	ss.ERAMComputers.CompletelyDeleteAircraft(ac)
 }
 

commit ffa2dfd223ed92bc4eb7e7df897e4a3daf56671e

sim: fix squawk code leak introduced in e7a9746eb9e801132a339
Fixes #538

pkg/sim/nas.go
@@ -1293,14 +1293,6 @@ func (ec *ERAMComputers) AddArrival(ac *av.Aircraft, facility string, fa STARSFa
 		return err
 	}
 
-	sq, err := artcc.CreateSquawk()
-	if err != nil {
-		return err
-	}
-
-	ac.FlightPlan.AssignedSquawk = sq
-	ac.Squawk = sq
-
 	artcc.AddFlightPlan(starsFP)
 
 	trk := TrackInformation{