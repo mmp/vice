93ea4bde304842a99d1e00ba23ccd58384d28efa
Matt Pharr <matt@pharr.org>
2025-03-09 17:42:12 -0700

    Don't list all the VFR airports in the launch config window

--- launch.go
+++ launch.go
@@ -706,10 +706,6 @@ func drawDepartureUI(lc *sim.LaunchConfig, p platform.Platform) (changed bool) {
 	if len(lc.VFRAirports) > 0 {
 		imgui.Separator()
 
-		aps := strings.Join(util.SortedMapKeys(lc.VFRAirports), ", ")
-		wrap, _ := util.WrapText("VFR departure airports: "+aps, 90, 4, true)
-		imgui.Text(wrap)
-
 		sumVFRRates := 0
 		for _, ap := range lc.VFRAirports {
 			r := float32(ap.VFRRateSum()) * lc.VFRDepartureRateScale
