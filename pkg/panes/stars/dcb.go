mmp/vice

commit 0c3d4ae7f2d88865ad85804b711f5b0d09fb2a27

stars: when command mode is set via keyboard, activate corresponding spinner (if any)

pkg/panes/stars/dcb.go
@@ -1057,7 +1057,8 @@ func (sp *STARSPane) drawDCBMouseDeltaButton(ctx *panes.Context, text string, co
 // events to the spinner.
 func (sp *STARSPane) drawDCBSpinner(ctx *panes.Context, spinner dcbSpinner, commandMode CommandMode, flags dcbFlags, buttonScale float32) {
 	active := sp.activeSpinner != nil && sp.activeSpinner.Equals(spinner)
-	if drawDCBButton(ctx, spinner.Label(), flags, buttonScale, active) && !active {
+	commandModeSelected := !active && sp.commandMode == commandMode
+	if (drawDCBButton(ctx, spinner.Label(), flags, buttonScale, active) && !active) || commandModeSelected {
 		sp.setCommandMode(ctx, commandMode)
 
 		sp.savedMousePosition = ctx.Mouse.Pos

commit 44a1648b7e8c5b5ac82e0292a8404fc9c99a87ba

stars: fix bug that broke BRITE and CHAR SIZE spinners
(Introduced in 0c3d4ae7f2d88865ad85804b711f5b0d09fb2a27)

pkg/panes/stars/dcb.go
@@ -1061,7 +1061,13 @@ func (sp *STARSPane) drawDCBMouseDeltaButton(ctx *panes.Context, text string, co
 // events to the spinner.
 func (sp *STARSPane) drawDCBSpinner(ctx *panes.Context, spinner dcbSpinner, commandMode CommandMode, flags dcbFlags, buttonScale float32) {
 	active := sp.activeSpinner != nil && sp.activeSpinner.Equals(spinner)
-	commandModeSelected := !active && sp.commandMode == commandMode
+	// Slightly tricky: if the user has selected a command mode via the
+	// keyboard and that command mode has a single associated spinner, then
+	// we want to automatically activate that one spinner.  However, two
+	// command modes have multiple associated spinners and so we don't want
+	// to do that trick then.
+	commandModeSelected := !active && sp.commandMode == commandMode &&
+		commandMode != CommandModeBriteSpinner && commandMode != CommandModeCharSizeSpinner
 	if (drawDCBButton(ctx, spinner.Label(), flags, buttonScale, active) && !active) || commandModeSelected {
 		sp.setCommandMode(ctx, commandMode)
 