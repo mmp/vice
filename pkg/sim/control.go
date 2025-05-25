mmp/vice

commit e46518fb489ca067820e764fa11e2d9978661b67

sim: fix bugs in AcceptHandoff
- ACID was missing in the posted event
- Incorrect "from" controller in the posted event
- Fix case where contact message might not be made

pkg/sim/control.go
@@ -728,12 +728,12 @@ func (s *Sim) AcceptHandoff(tcp string, acid ACID) error {
 				return nil
 			}
 			return av.ErrNotBeingHandedOffToMe
-
 		},
 		func(tcp string, fp *STARSFlightPlan, ac *Aircraft) []av.RadioTransmission {
 			s.eventStream.Post(Event{
 				Type:           AcceptedHandoffEvent,
-				FromController: fp.ControllingController,
+				ACID:           fp.ACID,
+				FromController: fp.TrackingController,
 				ToController:   tcp,
 			})
 
@@ -747,7 +747,7 @@ func (s *Sim) AcceptHandoff(tcp string, acid ACID) error {
 			if ac != nil {
 				haveTransferComms := slices.ContainsFunc(ac.Nav.Waypoints,
 					func(wp av.Waypoint) bool { return wp.TransferComms })
-				if !haveTransferComms && !s.isActiveHumanController(fp.ControllingController) {
+				if !haveTransferComms && !s.isActiveHumanController(fp.TrackingController) {
 					// For a handoff from a virtual controller, cue up a delayed
 					// contact message unless there's a point later in the route when
 					// comms are to be transferred.

commit b3368925ec345507c60a8ef4cd41ca2a8d1fcfac

sim: fix bug that broke contact messages after handoffs
Introduced in e46518fb4
Added an intermediate variable to make the logic more clear

pkg/sim/control.go
@@ -730,6 +730,8 @@ func (s *Sim) AcceptHandoff(tcp string, acid ACID) error {
 				ToController:   tcp,
 			})
 
+			previousTrackingController := fp.TrackingController
+
 			fp.HandoffTrackController = ""
 			fp.TrackingController = tcp
 			fp.LastLocalController = tcp
@@ -740,7 +742,7 @@ func (s *Sim) AcceptHandoff(tcp string, acid ACID) error {
 			if ac != nil {
 				haveTransferComms := slices.ContainsFunc(ac.Nav.Waypoints,
 					func(wp av.Waypoint) bool { return wp.TransferComms })
-				if !haveTransferComms && !s.isActiveHumanController(fp.TrackingController) {
+				if !haveTransferComms && !s.isActiveHumanController(previousTrackingController) {
 					// For a handoff from a virtual controller, cue up a delayed
 					// contact message unless there's a point later in the route when
 					// comms are to be transferred.