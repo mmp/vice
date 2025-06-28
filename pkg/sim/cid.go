b4580ae083bd9f0e4fa1c13c678fffc5d1678bc7
Michael <michaelt96099@gmail.com>
2025-06-27 22:42:48 -0400

    CIDs randomly assigned

--- pkg/sim/cid.go
+++ pkg/sim/cid.go
@@ -1,51 +1,61 @@
 package sim
 
-import "fmt"
-
 var preferredAlphas = []rune{'P', 'K', 'N', 'Y', 'T', 'V', 'F', 'R', 'C', 'D', 'E', 'W', 'A'}
 var nonPreferredAlphas = []rune{'M', 'X', 'L', 'J', 'U', 'B', 'G', 'Q', 'S', 'H', 'Z'}
 
 // CIDAllocator manages allocation of unique three-character CIDs.
 type CIDAllocator struct {
-	Available []string
-	Allocated map[string]struct{}
+	List [7]map[string]interface{}
 }
 
 func NewCIDAllocator() *CIDAllocator {
 	return &CIDAllocator{
-		Available: generateCIDList(),
-		Allocated: make(map[string]struct{}),
+		List: generateCIDList(),
 	}
 }
 
 // Allocate returns the next Available CID.
 func (c *CIDAllocator) Allocate() (string, error) {
-	if len(c.Available) == 0 {
-		return "", fmt.Errorf("no more CIDs Available")
+	var cid string 
+	Top: for i, m := range c.List {
+		var allValues []string 
+		for k := range m {
+			allValues = append(allValues, k)
+		}
+
+		for _, v := range allValues {
+			if k, ok := m[v]; ok && k == nil {
+				cid = v
+				c.List[i][v] = ""
+				break Top
+			}
+		}
 	}
-	cid := c.Available[0]
-	c.Available = c.Available[1:]
-	c.Allocated[cid] = struct{}{}
 	return cid, nil
 }
 
 // Release frees a CID so it can be reused.
 func (c *CIDAllocator) Release(cid string) {
-	if _, ok := c.Allocated[cid]; ok {
-		delete(c.Allocated, cid)
-		c.Available = append([]string{cid}, c.Available...)
+	for i, m := range c.List {
+		if _, ok := m[cid]; ok {
+			c.List[i][cid] = nil // Set to nil to mark as available
+			return
+		} 
 	}
 }
 
-func generateCIDList() []string {
+func generateCIDList() [7]map[string]interface{} {
 	digits := []rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
-	var codes []string
-	add := func(r1, r2, r3 rune) { codes = append(codes, string([]rune{r1, r2, r3})) }
+	var codes [7]map[string]interface{}
+	for i := range codes {
+		codes[i] = make(map[string]interface{})
+	}
+	add := func(r1, r2, r3 rune, i int) { codes[i][string([]rune{r1,r2,r3})] = nil }
 	// 1. ddd
 	for _, a := range digits {
 		for _, b := range digits {
 			for _, c := range digits {
-				add(a, b, c)
+				add(a, b, c, 0)
 			}
 		}
 	}
@@ -53,7 +63,7 @@ func generateCIDList() []string {
 	for _, a := range digits {
 		for _, b := range digits {
 			for _, l := range preferredAlphas {
-				add(a, b, l)
+				add(a, b, l ,1)
 			}
 		}
 	}
@@ -61,7 +71,7 @@ func generateCIDList() []string {
 	for _, a := range digits {
 		for _, l1 := range preferredAlphas {
 			for _, l2 := range preferredAlphas {
-				add(a, l1, l2)
+				add(a, l1, l2, 2)
 			}
 		}
 	}
@@ -69,7 +79,7 @@ func generateCIDList() []string {
 	for _, a := range digits {
 		for _, l := range preferredAlphas {
 			for _, b := range digits {
-				add(a, l, b)
+				add(a, l, b, 3)
 			}
 		}
 	}
@@ -77,7 +87,7 @@ func generateCIDList() []string {
 	for _, a := range digits {
 		for _, b := range digits {
 			for _, l := range nonPreferredAlphas {
-				add(a, b, l)
+				add(a, b, l, 4)
 			}
 		}
 	}
@@ -85,15 +95,15 @@ func generateCIDList() []string {
 	for _, d := range digits {
 		for _, a1 := range nonPreferredAlphas {
 			for _, a2 := range nonPreferredAlphas {
-				add(d, a1, a2)
+				add(d, a1, a2, 5)
 			}
 			for _, a2 := range preferredAlphas {
-				add(d, a1, a2)
+				add(d, a1, a2, 5)
 			}
 		}
 		for _, a1 := range preferredAlphas {
 			for _, a2 := range nonPreferredAlphas {
-				add(d, a1, a2)
+				add(d, a1, a2, 5)
 			}
 		}
 	}
@@ -101,7 +111,7 @@ func generateCIDList() []string {
 	for _, a := range digits {
 		for _, l := range nonPreferredAlphas {
 			for _, b := range digits {
-				add(a, l, b)
+				add(a, l, b, 6)
 			}
 		}
 	}
