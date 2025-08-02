8583e494358162934df4b8bf8279f8fec9bbee66
Matt Pharr <matt@pharr.org>
2025-08-02 16:43:44 -0700

    wx: custom case for single layer in BlendWindLayers()

--- pkg/aviation/weather.go
+++ pkg/aviation/weather.go
@@ -95,6 +95,10 @@ func ParseWindLayers(str string, e *util.ErrorLogger) []WindLayer {
 }
 
 func BlendWindLayers(wts []float32, layers []WindLayer) WindLayer {
+	if len(layers) == 1 {
+		return layers[0]
+	}
+
 	var alt, sumwt float32
 	var vspd, vgst [2]float32
 	for i, wt := range wts {
