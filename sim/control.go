04718cf2fe10382aea84e8e9453ec5086cf6ba35
Michael <michaelt96099@gmail.com>
2025-12-12 01:02:52 -0500

    gofmt

--- sim/control.go
+++ sim/control.go
@@ -1530,7 +1530,7 @@ func (s *Sim) processEnqueued() {
 				if ac, ok := s.Aircraft[oc.ADSBCallsign]; ok {
 					s.lg.Info("departing on course", slog.String("adsb_callsign", string(ac.ADSBCallsign)),
 						slog.Int("final_altitude", ac.FlightPlan.Altitude))
-						// Clear temporary altitude 
+					// Clear temporary altitude
 					s.State.Tracks[ac.ADSBCallsign].FlightPlan.InterimAlt = 0
 					s.State.Tracks[ac.ADSBCallsign].FlightPlan.InterimType = 0
 					ac.DepartOnCourse(s.lg)
