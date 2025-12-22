54093c78b8efd6a9636aadfea01d0359c404b8cc
Matt Pharr <matt@pharr.org>
2025-12-22 07:09:51 -0800

    sim: handle full consolidation of tracks being handed off to user

--- sim/consolidation.go
+++ sim/consolidation.go
@@ -392,6 +392,10 @@ func (s *Sim) ConsolidateTCP(receivingTCW TCW, sendingTCP TCP, consType Consolid
 		for _, ac := range s.Aircraft {
 			if ac.NASFlightPlan != nil && slices.Contains(transferredTCPs, ac.NASFlightPlan.TrackingController) {
 				ac.NASFlightPlan.OwningTCW = receivingTCW
+				if ac.NASFlightPlan.HandoffController != "" && s.State.UserControlsPosition(ac.NASFlightPlan.HandoffController) {
+					// It's being flashed to us but was then consolidated; the handoff is now irrelevant...
+					ac.NASFlightPlan.HandoffController = ""
+				}
 				s.lg.Infof("transferred track %s ownership to %s", ac.NASFlightPlan.ACID, receivingTCW)
 			}
 		}
