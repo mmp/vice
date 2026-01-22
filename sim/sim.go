mmp/vice

commit ba988bf3f7bcbdcc764f64eefb0092028d814508

feat(fe): allow departure contact location to be defined in routes (#757)
* refactor(sim): move contact departure logic to its own method

* feat(aviation): only check for TC before human handoff in arrivals

* feat: allow departure contact location to be defined in routes

* fix(sim): stop aircraft calling in twice

* fix(sim): make emergencies contact departure immediately

* docs: update documentation for new /tc functionality

* fix(sim): change fp.ControllingController to ac.ControllerFrequency (see 2eaa509)

* fix(sim): fix arrival emergencies being broken

The new logic which prevents an emergency from proceeding if the aircraft has not yet contacted departure would also stop all arrival emergencies from working. Additionally, this commit adds a comment documenting the -1 & 0 options for Aircraft#DepartureContactAltitude

* style: run gofmt

sim/sim.go
@@ -1129,17 +1129,22 @@ func (s *Sim) updateState() {
 					sfp := ac.NASFlightPlan
 
 					if passedWaypoint.TransferComms {
-						// We didn't enqueue this before since we knew an
-						// explicit comms handoff was coming so go ahead and
-						// send them to the controller's frequency. Note that
-						// we use InboundHandoffController and not
-						// ac.TrackingController, since the human controller
-						// may have already flashed the track to a virtual
-						// controller.
-						ctrl := s.State.ResolveController(sfp.InboundHandoffController)
-						// Make sure they've bought the handoff.
-						if ctrl != sfp.HandoffController {
-							s.enqueueControllerContact(ac.ADSBCallsign, ctrl, 0 /* no delay */)
+						// This is a departure that hasn't contacted the departure controller yet, do it here
+						if ac.IsDeparture() && ac.DepartureContactAltitude == 0 {
+							s.contactDeparture(ac)
+						} else {
+							// We didn't enqueue this before since we knew an
+							// explicit comms handoff was coming so go ahead and
+							// send them to the controller's frequency. Note that
+							// we use InboundHandoffController and not
+							// ac.TrackingController, since the human controller
+							// may have already flashed the track to a virtual
+							// controller.
+							ctrl := s.State.ResolveController(sfp.InboundHandoffController)
+							// Make sure they've bought the handoff.
+							if ctrl != sfp.HandoffController {
+								s.enqueueControllerContact(ac.ADSBCallsign, ctrl, 0 /* no delay */)
+							}
 						}
 					}
 
@@ -1224,7 +1229,7 @@ func (s *Sim) updateState() {
 			}
 
 			// Possibly contact the departure controller
-			if ac.DepartureContactAltitude != 0 && ac.Nav.FlightState.Altitude >= ac.DepartureContactAltitude {
+			if (ac.DepartureContactAltitude > 0 && ac.Nav.FlightState.Altitude >= ac.DepartureContactAltitude) || (ac.DepartureContactAltitude == 0 && ac.EmergencyState != nil) {
 				fp := ac.NASFlightPlan
 				if fp == nil {
 					fp = s.STARSComputer.lookupFlightPlanBySquawk(ac.Squawk)
@@ -1242,20 +1247,7 @@ func (s *Sim) updateState() {
 						// Use the original InboundHandoffController position for the radio event,
 						// not the resolved position. This ensures TCWControlsPosition checks
 						// correctly match when the user has that position consolidated.
-						tcp := fp.InboundHandoffController
-						s.lg.Debug("contacting departure controller", slog.String("tcp", string(tcp)))
-
-						rt := ac.Nav.DepartureMessage(ac.ReportDepartureHeading)
-						s.postContactTransmission(ac.ADSBCallsign, tcp, *rt)
-
-						// Clear this out so we only send one contact message
-						ac.DepartureContactAltitude = 0
-
-						// Only after we're on frequency can the controller start
-						// issuing control commands.. (Note that track may have
-						// already been handed off to the next controller at this
-						// point.)
-						ac.ControllerFrequency = ControlPosition(tcp)
+						s.contactDeparture(ac)
 					}
 				}
 			}
@@ -1484,6 +1476,31 @@ func (s *Sim) sendFullFlightFollowingRequest(ac *Aircraft, tcp TCP) {
 	rt.Type = av.RadioTransmissionContact
 
 	s.postContactTransmission(ac.ADSBCallsign, tcp, *rt)
+}
+
+func (s *Sim) contactDeparture(ac *Aircraft) {
+	fp := ac.NASFlightPlan
+
+	tcp := fp.InboundHandoffController
+	s.lg.Debug("contacting departure controller", slog.String("tcp", string(tcp)))
+
+	rt := ac.Nav.DepartureMessage(ac.ReportDepartureHeading)
+	s.postContactTransmission(ac.ADSBCallsign, tcp, *rt)
+
+	// Clear this out so we only send one contact message
+	ac.DepartureContactAltitude = 0
+
+	// Only after we're on frequency can the controller start
+	// issuing control commands.. (Note that track may have
+	// already been handed off to the next controller at this
+	// point.)
+	ac.ControllerFrequency = ControlPosition(tcp)
+
+	// Queued emergencies can now proceed
+	if ac.EmergencyState != nil && ac.EmergencyState.CurrentStage == -1 {
+		ac.EmergencyState.CurrentStage = 0
+		s.runEmergencyStage(ac)
+	}
 }
 
 func (s *Sim) isRadarVisible(ac *Aircraft) bool {

aviation/airport.go
@@ -370,6 +370,10 @@ func (ap *Airport) PostDeserialize(icao string, loc Locator, nmPerLongitude floa
 
 			for i := range route.Waypoints {
 				route.Waypoints[i].OnSID = true
+
+				if route.Waypoints[i].TransferComms {
+					route.WaitToContactDeparture = true
+				}
 			}
 
 			route.Waypoints.CheckDeparture(e, controlPositions, checkScratchpad)
@@ -719,6 +723,8 @@ type ExitRoute struct {
 	HandoffController ControlPosition `json:"handoff_controller"`
 	// optional, the initial tracking controller for the departure.
 	DepartureController ControlPosition `json:"departure_controller"`
+
+	WaitToContactDeparture bool // whether the aircraft waits until a /TC point to contact departure
 }
 
 // FinalHeading returns the final heading from the exit route's waypoints.

aviation/route.go
@@ -371,7 +371,6 @@ func (wa WaypointArray) CheckDeparture(e *util.ErrorLogger, controllers map[Cont
 func (wa WaypointArray) checkBasics(e *util.ErrorLogger, controllers map[ControlPosition]*Controller, checkScratchpad func(string) bool) {
 	defer e.CheckDepth(e.CurrentDepth())
 
-	haveHO := false
 	for i, wp := range wa {
 		e.Push(wp.Fix)
 		if wp.Speed < 0 || wp.Speed > 300 {
@@ -407,8 +406,6 @@ func (wa WaypointArray) checkBasics(e *util.ErrorLogger, controllers map[Control
 		}
 
 		if wp.HumanHandoff {
-			haveHO = true
-
 			// Check if any subsequent waypoints have a HandoffController
 			for _, wfut := range wa[i:] {
 				if wfut.HandoffController != "" {
@@ -416,10 +413,6 @@ func (wa WaypointArray) checkBasics(e *util.ErrorLogger, controllers map[Control
 					break
 				}
 			}
-		}
-
-		if wp.TransferComms && !haveHO {
-			e.ErrorString("Must have /ho to handoff to a human controller at a waypoint prior to /tc")
 		}
 
 		if i == 0 && wp.Shift > 0 {
@@ -470,6 +463,7 @@ func (wa WaypointArray) CheckArrival(e *util.ErrorLogger, ctrl map[ControlPositi
 
 	wa.checkBasics(e, ctrl, checkScratchpad)
 	wa.checkDescending(e)
+	haveHO := false
 
 	for _, wp := range wa {
 		e.Push(wp.Fix)
@@ -478,6 +472,12 @@ func (wa WaypointArray) CheckArrival(e *util.ErrorLogger, ctrl map[ControlPositi
 		}
 		if wp.ClearApproach && !approachAssigned {
 			e.ErrorString("/clearapp specified but no approach has been assigned")
+		}
+		if wp.HumanHandoff {
+			haveHO = true
+		}
+		if wp.TransferComms && !haveHO {
+			e.ErrorString("Must have /ho to handoff to a human controller at a waypoint prior to /tc")
 		}
 		e.Pop()
 	}

sim/aircraft.go
@@ -80,8 +80,8 @@ type Aircraft struct {
 	GoAroundDistance *float32
 
 	// Departure related state
-	DepartureContactAltitude float32
-	ReportDepartureHeading   bool // true if runway has multiple exit headings
+	DepartureContactAltitude float32 // 0 = waiting for /tc point, -1 = already contacted departure
+	ReportDepartureHeading   bool    // true if runway has multiple exit heading
 
 	// The controller who gave approach clearance
 	ApproachTCP TCP

sim/emergency.go
@@ -205,7 +205,7 @@ func (s *Sim) triggerEmergency(idx int) bool {
 
 	// Trigger the first stage immediately unless it's an external arrival that hasn't been handed
 	// off yet. Those trigger when controller contact happens in processEnququed.
-	if ac.IsAssociated() {
+	if ac.IsAssociated() && (!ac.IsDeparture() || ac.DepartureContactAltitude != 0) {
 		s.runEmergencyStage(ac)
 	} else {
 		// Mark as dormant until handoff; -1 signals the emergency should activate

sim/spawn_departures.go
@@ -562,8 +562,12 @@ func (s *Sim) assignDepartureController(ac *Aircraft, nasFp *NASFlightPlan,
 	}
 
 	// Set altitude at which aircraft will contact departure control
-	ac.DepartureContactAltitude = ac.Nav.FlightState.DepartureAirportElevation + 500 + float32(s.Rand.Intn(500))
-	ac.DepartureContactAltitude = min(ac.DepartureContactAltitude, float32(ac.FlightPlan.Altitude))
+	if exitRoute.WaitToContactDeparture {
+		ac.DepartureContactAltitude = 0
+	} else {
+		ac.DepartureContactAltitude = ac.Nav.FlightState.DepartureAirportElevation + 500 + float32(s.Rand.Intn(500))
+		ac.DepartureContactAltitude = min(ac.DepartureContactAltitude, float32(ac.FlightPlan.Altitude))
+	}
 
 	nasFp.TrackingController = pos
 	nasFp.OwningTCW = s.tcwForPosition(pos)

website/index.html
@@ -6650,7 +6650,8 @@ SBA, SNA, SCTLA, DCA, Del Rey, SAN, PCT SHD (Ketan K), TPA (Nelson T), AAC, BOS 
               </li>
               <li> <code>/tc</code>: "transfer comms": after passing a <code>/ho</code> fix, normally aircraft call in
                 a few seconds after the human controller accepts the handoff.  However, if <code>/tc</code> is specified
-                at a later fix in the route, they won't contact the controller until reaching that fix.</li>
+                at a later fix in the route, they won't contact the controller until reaching that fix. Additionally, this can be used in departure routes to signify a 
+                specific fix at which aircraft should contact the departure controller.</li>
               <li> <code>/po</code><i>TCP id</i>: when the aircraft passes the fix
                 and if it is under the control of a virtual controller,
                 issue a point out to the controller identified by the given TCP id (i.e., the

commit ad3e718ecdfee91e54d2f1e651ccc1714a7b2042

fix: prevent emergency aircraft from repeatedly calling in
Fixed two issues introduced in ba988bf:
1. contactDeparture() was setting DepartureContactAltitude to 0 instead
   of -1 after contact, causing the emergency contact condition to keep
   triggering on every update
2. Added IsDeparture() check to prevent arrivals with emergencies from
   incorrectly triggering the departure contact logic

sim/sim.go
@@ -1229,7 +1229,7 @@ func (s *Sim) updateState() {
 			}
 
 			// Possibly contact the departure controller
-			if (ac.DepartureContactAltitude > 0 && ac.Nav.FlightState.Altitude >= ac.DepartureContactAltitude) || (ac.DepartureContactAltitude == 0 && ac.EmergencyState != nil) {
+			if ac.IsDeparture() && ((ac.DepartureContactAltitude > 0 && ac.Nav.FlightState.Altitude >= ac.DepartureContactAltitude) || (ac.DepartureContactAltitude == 0 && ac.EmergencyState != nil)) {
 				fp := ac.NASFlightPlan
 				if fp == nil {
 					fp = s.STARSComputer.lookupFlightPlanBySquawk(ac.Squawk)
@@ -1487,8 +1487,8 @@ func (s *Sim) contactDeparture(ac *Aircraft) {
 	rt := ac.Nav.DepartureMessage(ac.ReportDepartureHeading)
 	s.postContactTransmission(ac.ADSBCallsign, tcp, *rt)
 
-	// Clear this out so we only send one contact message
-	ac.DepartureContactAltitude = 0
+	// Mark as already contacted so we only send one contact message
+	ac.DepartureContactAltitude = -1
 
 	// Only after we're on frequency can the controller start
 	// issuing control commands.. (Note that track may have