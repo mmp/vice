mmp/vice

commit 7c32515e932162b855ad202aeceda2ee263cd430

stars: add support for multiple monitor color schemes.
This involved cleaning up a lot of hard-coded color values in the STARS code,
now all going through a new MonitorColors struct.

Added support for the new MDM3 and MDM4 color schemes, controllable both
through facility JSON as well as the settings UI.

stars/datablock.go
@@ -1083,44 +1083,44 @@ func (sp *STARSPane) trackDatablockColorBrightness(ctx *panes.Context, trk sim.T
 
 	if state.IsSelected {
 		// middle button selected
-		color = sp.Colors.SelectedAircraft
+		color = sp.Colors.SelectedDatablock
 	} else if trk.IsUnassociated() {
-		color = sp.Colors.UntrackedAircraft
+		color = sp.Colors.UnownedDatablock
 	} else {
 		sfp := trk.FlightPlan
 		if _, ok := sp.ForceQLACIDs[sfp.ACID]; ok {
 			// Check if we're the controller being ForceQL
-			color = sp.Colors.TrackAlert
+			color = sp.Colors.AlertDatablock
 		} else if state.PointOutAcknowledged || state.ForceQL {
 			// Ack'ed point out to us (but not cleared) or force quick look.
-			color = sp.Colors.TrackAlert
+			color = sp.Colors.AlertDatablock
 		} else if inboundPointOut {
 			// Pointed out to us.
-			color = sp.Colors.TrackAlert
+			color = sp.Colors.AlertDatablock
 		} else if state.DatablockAlert {
-			color = sp.Colors.TrackAlert
+			color = sp.Colors.AlertDatablock
 		} else if ctx.UserOwnsFlightPlan(sfp) {
 			// we own the track
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else if ctx.UserControlsPosition(sfp.RedirectedHandoff.OriginalOwner) ||
 			ctx.UserControlsPosition(sfp.RedirectedHandoff.RedirectedTo) {
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else if ctx.UserControlsPosition(sfp.HandoffController) &&
 			!ctx.UserControlsPosition(sfp.RedirectedHandoff.GetLastRedirector()) {
 			// flashing white if it's being handed off to us.
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else if state.OutboundHandoffAccepted {
 			// we handed it off, it was accepted, but we haven't yet acknowledged
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else if ps.QuickLookAll && ps.QuickLookAllIsPlus {
 			// quick look all plus
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else if ps.QuickLookTCPs[string(ctx.PrimaryTCPForTCW(sfp.OwningTCW))] {
 			// individual quicklook plus controller
-			color = sp.Colors.TrackedAircraft
+			color = sp.Colors.OwnedDatablock
 		} else {
 			// other controller owns, no special case applies
-			color = sp.Colors.UntrackedAircraft
+			color = sp.Colors.UnownedDatablock
 		}
 	}
 
@@ -1355,7 +1355,7 @@ func (sp *STARSPane) getDatablockAlerts(ctx *panes.Context, trk sim.Track, dbtyp
 		}
 		added[s] = nil
 
-		color := util.Select(red, sp.Colors.TextAlert, sp.Colors.TextWarning)
+		color := util.Select(red, sp.Colors.AlertDatablock, sp.Colors.CautionDatablock)
 		if len(alerts) > 0 {
 			alerts = append(alerts, dbChar{ch: '/', color: color})
 		}

radar/tools.go
@@ -13,11 +13,6 @@ import (
 
 type Brightness int
 
-func (b Brightness) RGB() renderer.RGB {
-	v := float32(b) / 100
-	return renderer.RGB{v, v, v}
-}
-
 func (b Brightness) ScaleRGB(r renderer.RGB) renderer.RGB {
 	return r.Scale(float32(b) / 100)
 }

radar/weather.go
@@ -252,7 +252,8 @@ func reverseStippleBytes(stipple [32]uint32) [32]uint32 {
 }
 
 // Draw draws the current weather radar data, if available.
-func (w *WeatherRadar) Draw(ctx *panes.Context, hist int, intensity float32, contrast float32,
+func (w *WeatherRadar) Draw(ctx *panes.Context, hist int, intensity float32,
+	wxColors [2]renderer.RGB, wxStippleColor renderer.RGB,
 	active [NumWxLevels]bool, transforms ScopeTransformations, cb *renderer.CommandBuffer) {
 	w.mu.Lock(ctx.Lg)
 	defer w.mu.Unlock(ctx.Lg)
@@ -287,9 +288,7 @@ func (w *WeatherRadar) Draw(ctx *panes.Context, hist int, intensity float32, con
 	transforms.LoadLatLongViewingMatrices(cb)
 	for i := range w.cb[hist] {
 		if active[i] && w.cb[hist][i] != nil {
-			// RGBs from STARS Manual, B-5
-			baseColor := util.Select(i < 3,
-				renderer.RGBFromUInt8(37, 77, 77), renderer.RGBFromUInt8(100, 100, 51))
+			baseColor := util.Select(i < 3, wxColors[0], wxColors[1])
 			cb.SetRGB(baseColor.Scale(intensity))
 			cb.Call(*w.cb[hist][i])
 
@@ -305,7 +304,7 @@ func (w *WeatherRadar) Draw(ctx *panes.Context, hist int, intensity float32, con
 				cb.PolygonStipple(reverseStippleBytes(wxStippleDense))
 			}
 			// Draw the same quads again, just with a different color and stippled.
-			cb.SetRGB(renderer.RGB{contrast, contrast, contrast})
+			cb.SetRGB(wxStippleColor)
 			cb.Call(*w.cb[hist][i])
 			cb.DisablePolygonStipple()
 		}

sim/stars.go
@@ -374,6 +374,7 @@ type FacilityAdaptation struct {
 	SingleCharAIDs    map[string]string             `json:"single_char_aids" scope:"stars"` // Char to airport. TODO: Check if this is for ERAM as well.
 	KeepLDB           bool                          `json:"keep_ldb" scope:"stars"`
 	FullLDBSeconds    int                           `json:"full_ldb_seconds" scope:"stars"`
+	Monitor           string                        `json:"monitor" scope:"stars"`
 
 	SSRCodes av.LocalSquawkCodePoolSpecifier `json:"ssr_codes" scope:"stars"`
 
@@ -1494,6 +1495,15 @@ func (fa *FacilityAdaptation) PostDeserialize(loc av.Locator, controlledAirports
 		if !fa.CheckScratchpad(sp) {
 			e.ErrorString("%s: invalid scratchpad in \"scratchpads\"", sp)
 		}
+	}
+
+	switch fa.Monitor {
+	// Ugly: we need to keep this in sync with colorSets in stars/stars.go
+	case "":
+		fa.Monitor = "legacy" // default
+	case "legacy", "mdm3", "mdm4":
+	default:
+		e.ErrorString(`%s: invalid value for "monitor": must be "legacy", "mdm3", or "mdm4"`, fa.Monitor)
 	}
 
 	makeCircleAirportFilters := func(id string, description string, radius float32,

stars/lists.go
@@ -295,7 +295,7 @@ func (sp *STARSPane) handleListDrag(ctx *panes.Context, bounds math.Extent2D, po
 			P0: math.Sub2f(cursorPos, offset),
 			P1: math.Add2f(math.Sub2f(cursorPos, offset), size),
 		}
-		sp.drawListFrameColor(ctx, movedBounds, renderer.RGB{1, 1, 1}, ld)
+		sp.drawListFrameColor(ctx, movedBounds, sp.Colors.ListFrame, ld)
 
 		// Second middle click outside original bounds finishes the move
 		if ctx.Mouse.Clicked[platform.MouseButtonTertiary] && !sp.movingListBounds.Inside(ctx.Mouse.Pos) {
@@ -1310,7 +1310,7 @@ func (sp *STARSPane) drawCoordinationLists(ctx *panes.Context, paneExtent math.E
 	for i, cl := range fa.CoordinationLists {
 		listStyle := renderer.TextStyle{
 			Font:  font,
-			Color: ps.Brightness.Lists.ScaleRGB(util.Select(cl.YellowEntries, renderer.RGB{1, 1, 0}, sp.Colors.List)),
+			Color: ps.Brightness.Lists.ScaleRGB(util.Select(cl.YellowEntries, sp.Colors.TextWarning, sp.Colors.List)),
 		}
 		dimStyle := renderer.TextStyle{
 			Font:  font,

stars/tools.go
@@ -161,7 +161,7 @@ func (sp *STARSPane) drawHighlighted(ctx *panes.Context, transforms radar.ScopeT
 	// "The color of the blinking square is the same as that for blinking
 	// data block information"(?)
 	ps := sp.currentPrefs()
-	color := ps.Brightness.FullDatablocks.ScaleRGB(sp.Colors.UntrackedAircraft)
+	color := ps.Brightness.FullDatablocks.ScaleRGB(sp.Colors.UnownedDatablock)
 	halfSeconds := ctx.Now.UnixMilli() / 500
 	blinkDim := halfSeconds&1 == 0
 	if blinkDim {
@@ -191,7 +191,7 @@ func (sp *STARSPane) drawVFRAirports(ctx *panes.Context, transforms radar.ScopeT
 	defer renderer.ReturnLinesDrawBuilder(ld)
 
 	ps := sp.currentPrefs()
-	color := ps.Brightness.Lines.RGB()
+	color := ps.Brightness.Lines.ScaleRGB(sp.Colors.RangeBearingLine) // arbitrary white-ish...
 	style := renderer.TextStyle{
 		Font:  sp.systemFont(ctx, ps.CharSize.Tools),
 		Color: color,
@@ -221,7 +221,7 @@ func (sp *STARSPane) drawRBLs(ctx *panes.Context, transforms radar.ScopeTransfor
 	defer renderer.ReturnColoredLinesDrawBuilder(ld)
 
 	ps := sp.currentPrefs()
-	color := ps.Brightness.Lines.RGB() // check
+	color := ps.Brightness.Lines.ScaleRGB(sp.Colors.RangeBearingLine)
 	style := renderer.TextStyle{
 		Font:  sp.systemFont(ctx, ps.CharSize.Tools),
 		Color: color,
@@ -310,7 +310,7 @@ func (sp *STARSPane) drawMinSep(ctx *panes.Context, transforms radar.ScopeTransf
 	}
 
 	ps := sp.currentPrefs()
-	color := ps.Brightness.Lines.RGB()
+	color := ps.Brightness.Lines.ScaleRGB(sp.Colors.PTL) // same color for min sep as PTL
 
 	s0, ok0 := sp.TrackState[trk0.ADSBCallsign]
 	s1, ok1 := sp.TrackState[trk1.ADSBCallsign]
@@ -675,7 +675,7 @@ func (sp *STARSPane) drawPTLs(ctx *panes.Context, transforms radar.ScopeTransfor
 	ld := renderer.GetColoredLinesDrawBuilder()
 	defer renderer.ReturnColoredLinesDrawBuilder(ld)
 
-	color := ps.Brightness.Lines.RGB()
+	color := ps.Brightness.Lines.ScaleRGB(sp.Colors.PTL)
 
 	for _, trk := range sp.visibleTracks {
 		state := sp.TrackState[trk.ADSBCallsign]
@@ -908,7 +908,7 @@ func (sp *STARSPane) drawWind(ctx *panes.Context, transforms radar.ScopeTransfor
 
 	ps := sp.currentPrefs()
 	font := sp.systemFont(ctx, ps.CharSize.Tools)
-	color := ps.Brightness.Lines.RGB()
+	color := ps.Brightness.Lines.ScaleRGB(sp.Colors.RangeBearingLine) // arbitrary white-ish...
 
 	const arrowLength = 25
 	const arrowheadSize = 8

commit e5921745f3b621284c9dc777034daf767f0c374f

stars: fix bugs in datablock color selection
Introduced in 7c32515e932162b855ad202aeceda2ee263cd430...

stars/datablock.go
@@ -1090,15 +1090,16 @@ func (sp *STARSPane) trackDatablockColorBrightness(ctx *panes.Context, trk sim.T
 		sfp := trk.FlightPlan
 		if _, ok := sp.ForceQLACIDs[sfp.ACID]; ok {
 			// Check if we're the controller being ForceQL
-			color = sp.Colors.AlertDatablock
+			color = sp.Colors.CautionDatablock
 		} else if state.PointOutAcknowledged || state.ForceQL {
 			// Ack'ed point out to us (but not cleared) or force quick look.
-			color = sp.Colors.AlertDatablock
+			color = sp.Colors.CautionDatablock
 		} else if inboundPointOut {
 			// Pointed out to us.
-			color = sp.Colors.AlertDatablock
+			color = sp.Colors.CautionDatablock
 		} else if state.DatablockAlert {
-			color = sp.Colors.AlertDatablock
+			// This is indeed supposed to be CautionDatablock, not AlertDatablock(!).
+			color = sp.Colors.CautionDatablock
 		} else if ctx.UserOwnsFlightPlan(sfp) {
 			// we own the track
 			color = sp.Colors.OwnedDatablock